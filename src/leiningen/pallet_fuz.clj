b08a79e5508bc052fbafc1a91ad90583553b3a4f
Jon Pither <jon.pither@gmail.com>
2013-03-11 21:03:14 +0000

    Split out the bulk of the code as a defplan, removed explicit calls to lein/setup etc, as the extends clause should already take care of this

--- src/leiningen/pallet_fuz.clj
+++ src/leiningen/pallet_fuz.clj
@@ -7,60 +7,58 @@
             [pallet.crate.automated-admin-user :as admin-user]
             [pallet.crate.java :as java]
             [pallet.crate.lein :as lein]
+            [pallet.crate :as crate]
             [pallet.crate.ssh-key :as ssh-key]
             [pallet.script :as script]
             [pallet.crate.git :as git]
             [clojure.java.io :as io]))
 
-;; todo: add hook for deployment code inserted via project.clj?
-
 (def pallet-fuz-upstart "upstart.conf")
+
+(crate/defplan install-application
+  [user pub-key pri-key git-url checkout-dir]
+  (action/package-manager :update)
+
+  ;; Setup deployment user
+  (action/user user :action :create :shell :bash :create-home true)
+  (ssh-key/install-key user "id_rsa" pri-key pub-key)
+
+  (pallet.action/with-action-options {:sudo-user user
+                                      :script-env {:HOME (str "/home/" user)}
+                                      :script-dir (str "/home/" user "/" checkout-dir)}
+
+    ;; We need to pull from github without any prompts
+    (action/remote-file (str "/home/" user "/.ssh/config")
+                        :content "Host *github.com\n    StrictHostKeyChecking no")
+
+    ;; Clone from git and cd in
+    (git/clone git-url :checkout-dir (str "/home/" user "/" checkout-dir))
+
+    ;; Trigger a download of lein
+    (action/exec-script (str "cd /home/" ~user "/" ~checkout-dir))
+    (lein/lein "version"))
+
+  (action/service-script "pallet-fuz"
+                         :template pallet-fuz-upstart
+                         :service-impl :upstart
+                         :literal true
+                         :values {:fuz-user user
+                                  :checkout-dir checkout-dir})
+
+  (action/service "pallet-fuz" :action :start :service-impl :upstart))
 
 (defn server-spec
   "Install lein and git, create a user, pull from github, fire up application"
   [user pub-key pri-key git-url checkout-dir]
   (api/server-spec
-   :extends [(java/java {}) (lein/leiningen {})]
+   :extends [(java/java {}) (lein/leiningen {}) (git/git {})]
    :phases
    {:bootstrap (api/plan-fn
                 (admin-user/automated-admin-user))
 
     :configure
     (api/plan-fn
-
-     (action/package-manager :update)
-     (lein/install-lein)
-     (git/install-git)
-
-     ;; Setup deployment user
-     (action/user user :action :create :shell :bash :create-home true)
-     (ssh-key/install-key user "id_rsa" pri-key pub-key)
-
-     (pallet.action/with-action-options {:sudo-user user
-                                         :script-env {:HOME (str "/home/" user)}
-                                         :script-dir (str "/home/" user "/" checkout-dir)}
-
-       ;; We need to pull from github without any prompts
-       (action/remote-file (str "/home/" user "/.ssh/config")
-                           :content "Host *github.com\n    StrictHostKeyChecking no")
-
-       ;; Clone from git and cd in
-       (git/clone git-url :checkout-dir (str "/home/" user "/" checkout-dir))
-
-       (action/exec-script (str "cd /home/" ~user "/" ~checkout-dir))
-
-       ;; Trigger a download of lein
-       (lein/lein "version"))
-
-     (action/service-script "pallet-fuz"
-                            :template pallet-fuz-upstart
-                            :service-impl :upstart
-                            :literal true
-                            :values {:fuz-user user
-                                     :checkout-dir checkout-dir})
-
-     (action/service "pallet-fuz" :action :start :service-impl :upstart)
-     )}))
+     (install-application user pub-key pri-key git-url checkout-dir))}))
 
 (defn setup [pallet]
   (println "Setting up...")
